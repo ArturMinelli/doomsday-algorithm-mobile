4e12f7417b1891b26a7786aa902bf559cc47d228
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-10 21:50:37 -0300

    Creating start button

--- src/screens/Trainer.tsx
+++ src/screens/Trainer.tsx
@@ -1,8 +1,34 @@
-import { Text, VStack } from "native-base";
+import { Text, VStack, Button, useTheme } from "native-base";
 
 export function Trainer() {
+  const { colors, sizes } = useTheme()
+
   return (
-    <VStack flex={1} bgColor="gray.600">
+    <VStack flex={1} alignItems="center" bgColor="gray.600">
+      <Button
+        width='75%'
+        marginTop={sizes[6]}
+        padding={sizes['1.5']}
+        borderRadius={99999}
+        borderWidth={sizes[1]}
+        borderColor={colors.green[500]}
+        bgColor={colors.gray[500]}
+        _pressed={{
+          opacity: 0.7,
+        }}
+      >
+        <Text
+          color={colors.gray[100]}
+          fontSize={sizes[9]}
+          fontWeight="extrabold"
+        >
+          Start
+        </Text>
+      </Button>
+
+      <VStack>
+
+      </VStack>
 
     </VStack>
   )
